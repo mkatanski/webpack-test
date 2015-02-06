9203cca82d9998d9d80098e389c19e55cdeb599d
Michal Katanski <mkatanski@nexway.com>
2015-02-06 14:34:59 +0100

    Multiple lazy loaded entries

--- app/main.js
+++ app/main.js
@@ -1,4 +1,30 @@
 
 var React = require('react');
-var AppComponent = require('./AppComponent.js');
-React.render(<AppComponent/>, document.body);
+
+// We create a function that will lazy load modules based on the current hash
+var resolveRoute = function () {
+
+    // If no hash or hash is '#' we lazy load the Home component
+    if (!location.hash || location.hash.length === 1) {
+        require.ensure([], function () {
+            var Home = require('./Home.js');
+            React.render(Home(), document.getElementById('app'));
+        });
+
+        // Or if route is #admin we lazy load that
+    } else if (location.hash === '#admin') {
+        require.ensure([], function () {
+            var Admin = require('./Admin.js');
+            React.render(Admin(), document.getElementById('app'));
+        });
+    }
+
+};
+
+// Resolve route on hash change
+window.onhashchange = resolveRoute;
+
+console.log('Test');
+
+// Resolve current route
+resolveRoute();
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,5 @@
-// Add WebPack to use the included CommonsChunkPlugin
-var webpack = require('webpack');
-var bower_dir = __dirname + '/bower_components';
-
-var config = {
-    addVendor: function (name, path) {
-        this.resolve.alias[name] = path;
-        this.module.noParse.push(new RegExp('^' + name + '$'));
-    },
-
-    // We split the entry into two specific chunks. Our app and vendors. Vendors
-    // specify that react should be part of that chunk
-    entry: {
-        app: ['./app/main.js'],
-        vendors: ['react']
-    },
-    resolve: { alias: {} },
-
-    // We add a plugin called CommonsChunkPlugin that will take the vendors chunk
-    // and create a vendors.js file. As you can see the first argument matches the key
-    // of the entry, "vendors"
-    plugins: [
-        new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js')
-    ],
+module.exports = {
+    entry: ['./app/main.js'],
     output: {
         path: './build',
         filename: 'bundle.js'
@@ -33,7 +11,3 @@ var config = {
         ]
     }
 };
-
-config.addVendor('react', bower_dir + '/react/react.min.js');
-
-module.exports = config;
